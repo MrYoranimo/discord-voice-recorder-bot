a1412f33a86515ae2865e6565c1535530f371b21
Ad-Blokker <markblokker@outlook.com>
2020-04-08 16:04:01 +0200

    It also makes a thread that runs every 10 minutes and does a dice roll between 0 and 8 and if that dice roll results 5 it will join the channel and leave after 3 mins.
    fixes #2

--- src/main/java/com/yoranvulker/java/voicerecorderbot/VoiceRecorderBot.java
+++ src/main/java/com/yoranvulker/java/voicerecorderbot/VoiceRecorderBot.java
@@ -13,7 +13,45 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+
 public class VoiceRecorderBot implements EventListener {
+    public static class TenMinRun extends Thread {
+
+        private JDA jda;
+        public TenMinRun(JDA jda) {
+            this.jda = jda;
+        }
+
+        public void run() {
+            while (true) {
+                Random dice = new Random();
+                int diceRoll = dice.nextInt(8);
+                System.out.println("Thread 10 min run dicerole returns: " + diceRoll);
+
+                for (Guild guild : this.jda.getGuilds()) {
+                    for (VoiceChannel channel : guild.getVoiceChannels()) {
+                        List<Member> channelMembers = channel.getMembers().stream()
+                                .filter(member -> !member.getUser().isBot())
+                                .collect(Collectors.toList());
+
+                        if (channelMembers.size() > 0) {
+                            if(diceRoll == 5) guild.getAudioManager().openAudioConnection(channel);
+                            guild.getAudioManager().setReceivingHandler(new BotAudioReceiveHandler(jda));
+                            break;
+                        }
+                    }
+                }
+                try {
+                    sleep(180000);
+                    this.jda.getGuilds().forEach(guild -> guild.getAudioManager().closeAudioConnection());
+                    sleep(420000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     @Override
     public void onEvent(@Nonnull GenericEvent genericEvent) {
         if (genericEvent instanceof ReadyEvent) {
@@ -23,13 +61,18 @@ public class VoiceRecorderBot implements EventListener {
         if (genericEvent instanceof GuildVoiceUpdateEvent) {
             GuildVoiceUpdateEvent event = (GuildVoiceUpdateEvent) genericEvent;
 
-            if (event.getChannelJoined() != null) {
+            /* TODO: Implement the code below so that additionally to check every 10 min it also has a 1 in 20 chance
+                to join when a user joins a voicechannel, this already works.
+                What needs to be implemented is that it has to wait 3 min and then leave.
+            */
+           /* if (event.getChannelJoined() != null) {
                 VoiceChannel joinedChannel = event.getChannelJoined();
                 Guild joinedGuild = joinedChannel.getGuild();
                 Random dice = new Random();
-                int diceroll = dice.nextInt(3);
-                if (diceroll == 0) joinedGuild.getAudioManager().openAudioConnection(joinedChannel);
-            }
+                int diceRoll = dice.nextInt(20);
+                System.out.println("A person joined a voicechannel (1 in 20 chance that the bot joins) dicerole returns: " + diceRoll);
+                if (diceRoll == 10) joinedGuild.getAudioManager().openAudioConnection(joinedChannel);
+            }*/
 
             if (event.getChannelLeft() != null) {
                 VoiceChannel leftChannel = event.getChannelLeft();
@@ -63,21 +106,10 @@ public class VoiceRecorderBot implements EventListener {
 
             // wait until JDA has been initialized
             jda.awaitReady();
+
             System.out.printf("Connected to the following servers: %s%n", jda.getGuilds());
 
-            for (Guild guild : jda.getGuilds()) {
-                for (VoiceChannel channel : guild.getVoiceChannels()) {
-                    List<Member> channelMembers = channel.getMembers().stream()
-                            .filter(member -> !member.getUser().isBot())
-                            .collect(Collectors.toList());
-
-                    if (channelMembers.size() > 0) {
-                        guild.getAudioManager().openAudioConnection(channel);
-                        guild.getAudioManager().setReceivingHandler(new AivdAudioReceiveHandler(jda));
-                        break;
-                    }
-                }
-            }
+            new TenMinRun(jda).start();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
